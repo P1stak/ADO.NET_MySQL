867a7c4dbdaa1bf95351e0b5e8377c90d259c5ba
Alex_Pister <Lutcifer333@gmail.com>
2025-06-04 17:26:09 +0700

    added new class Courses for db

---
+++ ADO.NET test/Services/CoursesService.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_test.Services
+{
+    public class CoursesService
+    {
+        public static int GetTotalCount()
+        {
+            var connectionString = Constant.ConnectionString;
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                const string sqlQuery = "SELECT COUNT(*) FROM courses";
+
+                using (var command = new MySqlCommand(sqlQuery, connection))
+                {
+                    object result = command.ExecuteScalar();
+
+                    if (result != null) return Convert.ToInt32(result);
+
+                    else return 0;
+                }
+            }
+        }
+    }
+}
--- ADO.NET test/Services/UsersService.cs
+++ ADO.NET test/Services/UsersService.cs
@@ -70,5 +70,23 @@ namespace ADO.NET_test.Services
             }
             return null;
         }
+
+        public static int GetTotalCount()
+        {
+            using (var connection = new MySqlConnection(Constant.ConnectionString))
+            {
+                connection.Open();
+
+                const string sqlQuery= "SELECT COUNT(*) FROM users;";
+
+                using (var command = new MySqlCommand(sqlQuery, connection))
+                {
+                    object result = command.ExecuteScalar();
+
+                    if (result != null) return Convert.ToInt32(result);
+                    else return 0;
+                }
+            }
+        }
     }
 }
